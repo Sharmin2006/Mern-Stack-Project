1052fc7b5c15a7293d7fd3280cc4b1d8c03f5008
Sharmin kazi <sharminkazi328@gmail.com>
2025-03-06 10:34:40 +0530

    Constant Variable

--- JSFoundation/part1/changes.js
+++ JSFoundation/part1/changes.js
@@ -2,5 +2,5 @@ let gameName = "Ludo King";
 gameName = "Candy crush";
 console.log(gameName);
 const username = "sharminkazi";
-username = "sharmin"; //it will throw an error as we have already defined the username as constant variable so we cannot change the name of variable
+username = "sharmin"; //it will throw an error as we have already defined the username as constant variable so we cannot change the value of variable
 console.log(username);
