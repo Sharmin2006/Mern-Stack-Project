62f67ebb9dd6c1a3db29712a4900e74a3fe9fae2
Sharmin kazi <sharminkazi328@gmail.com>
2025-03-06 10:21:36 +0530

    Different types of Datatypes in JS

---
+++ JSFoundation/part1/datatype.js
@@ -0,0 +1,23 @@
+console.log("hello");
+// control+slash is a shortcut to comment out the line
+/* 
+Different types of Datatypes are:
+String
+Number
+Boolean
+Bigint
+Undefined
+Null:In programming languages Null means EMPTY and not ZERO
+Object
+Symbol
+*/
+var score = 102;
+//let score = 102;
+let name = "language.com";
+let isLogged = false;
+
+//object
+let languageTypes = ["Java", "Python", "C++", "C", "Html", "CSS", "Javascript"];
+let user = { firstname: "Sharmin", lastname: "kazi" };
+let getScore = score;
+console.log(getScore);
