1e5fa9f4b607c9f74e91cc229207383c678b93ea
Sharmin kazi <sharminkazi328@gmail.com>
2025-03-06 10:20:05 +0530

    Different operations in JS

---
+++ JSFoundation/part1/operations.js
@@ -0,0 +1,30 @@
+let score = 102;
+let bonus = 25;
+let totalScore = score + bonus;
+let addition = 4 + 5;
+let subtraction = 9 - 5;
+let mult = 4 * 5;
+let div = 4 / 2;
+let remainder = 4 % 2;
+let expo = 2 ** 3;
+
+let myscore = 110;
+myscore++;
+
+let credits = 58;
+credits--;
+
+//comparison operation
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+//logical operators
+//&&->and
+//||->or
+//!->not
+let isLoggedin = true;
+let ispaid = false;
+console.log(isLoggedin && ispaid);
