a651c0eb1914d29248b541976931c33d0aa873d3
Sharmin kazi <sharminkazi328@gmail.com>
2025-03-06 11:55:47 +0530

    Non Primitive Datatypes

---
+++ JSFoundation/part2/nonprimitive.js
@@ -0,0 +1,19 @@
+const username = {
+  "first name": "sharmin",
+  isLoggedin: true,
+};
+username.firstname = "Ms.SK";
+username.lastname = "kazi";
+
+console.log(username["first name"]);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);
+let today = new Date();
+console.log(today.getDate());
+//Array
+letanotherUser = ["sharmin", true];
+console.log(anotherUser[0]);
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
